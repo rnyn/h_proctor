14cb92032832dee35ac670f554e92514c779e5e1
rnyn <24571853+rnyn@users.noreply.github.com>
2018-11-20 11:33:28 +0100

    adding hero

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,15 @@
 body{
   font-family: 'Open Sans', sans-serif;
 }
+.container{
+  padding:10px;
+  margin: 0 auto;
+}
 </style>
 <template>
-
-<div>
+<section>
+<div class="container">
+  <img src="images/hp-hero.jpg">
 </div>
-
+</section>
 </template>

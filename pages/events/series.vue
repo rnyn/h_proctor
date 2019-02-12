1104de322512dfd0c918e99f008ae91d3f948521
rnyn <24571853+rnyn@users.noreply.github.com>
2019-02-12 19:09:26 +0100

    updated series, added links

--- pages/events/series.vue
+++ pages/events/series.vue
@@ -5,6 +5,23 @@
           <EventLevel />
   <div class="columns">
       <div class="column is-half">
+
+        <a href="http://diffrakt.space/en/?s=spellbound" target="_blank">
+
+        <div class="card">
+          <div class="image is-4by3">
+            <figure>
+        <img src="~/assets/spellbound.jpg">
+          </figure>
+        </div>
+          <div class="card-header">
+          <!-- <div class="card-header-title">Spellbound</div> -->
+        </div>
+          <div class="content"><blockquote><h5>Spellbound</h5><strong>diffrakt | zentrum für theoretische peripherie</strong><br>Berlin<br>2018-ongoing</blockquote></div>
+        </div>
+      </a>
+
+        <a href="https://weareunderthemoon.tumblr.com/" target="_blank">
       <div class="card">
         <div class="image is-4by3">
           <figure>
@@ -12,17 +29,17 @@
         </figure>
       </div>
         <div class="card-header">
-        <div class="card-header-title">Under the Moon</div>
+        <!-- <div class="card-header-title">Under the Moon</div> -->
   </div>
-        <div class="content"><blockquote><strong>MayDay Rooms</strong><br>London<br>2014-2016</blockquote></div>
-        <!-- <div class="media-content"><iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/178572763&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe></div> -->
+        <div class="content"><blockquote><h5>Under the Moon</h5><strong>MayDay Rooms</strong><br>London<br>2014-2016</blockquote></div>
       </div>
-
+    </a>
     </div>
 
 
 
       <div class="column is-half">
+        <a href="https://auhasardcineclub.blogspot.com/" target="_blank">
       <div class="card">
         <div class="card-image">
           <figure class="is-4by3">
@@ -30,20 +47,17 @@
         </figure>
       </div>
         <div class="card-header">
-          <div class="card-header-title">Au Hasard Cine Club  <a href="https://auhasardcineclub.blogspot.com/" target="_blank"><i class="fas fa-link"></i></a></div>
+          <!-- <div class="card-header-title">  <a href="https://auhasardcineclub.blogspot.com/" target="_blank"><i class="fas fa-link"></i></a></div> -->
             </div>
-            <div class="content"><blockquote><strong>Chapel Cinema, Bethnal Green / Birkbeck Cinema, Gordon Square </strong><br>London<br>2011-2012</blockquote>
+            <div class="content"><blockquote><h5>Au Hasard Cine Club</h5><strong>Chapel Cinema, Bethnal Green / Birkbeck Cinema, Gordon Square </strong><br>London<br>2011-2012</blockquote>
             </div>
 
 
-        <div class="media-content">
-          <div class="video-container">
-        <!-- <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/O45xCFvMKv0?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> -->
-      </div>
-    </div>
+
 
 
       </div>
+    </a>
     </div>
   </div>
   </div>

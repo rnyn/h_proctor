df113d848593eb0d005d989c713bffcd3d6e335f
rnyn <rnyn@users.noreply.github.com>
2021-04-19 14:39:51 +0200

    font-display: swap;

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,31 @@
-
 <style>
-body{
-  font-family: 'Alegreya', serif;
- }
-
-#homediv{
- max-height:100%;
+body {
+  font-family: "Alegreya", serif;
+  font-display: swap;
 }
-#homeimg{
 
+#homediv {
+  max-height: 100%;
+}
+#homeimg {
   border: 1.5px solid;
-  max-width:512px;
+  max-width: 512px;
 }
-#homeimg-caption{
-
-  font-family: 'Alegreya SC', serif;
-
+#homeimg-caption {
+  font-family: "Alegreya SC", serif;
+  font-display: swap;
 }
 </style>
 
-
 <template>
-<section class="section">
-  <div class="content" id="homediv">
-<figure class="image">
-  <img src="~/assets/moscow3.jpg" id="homeimg">
-  <figcaption id="homeimg-caption">Revolutionary psychologies, communist neurologies, red therapies</figcaption>
-</figure>
-
-  </div>
-
-</section>
+  <section class="section">
+    <div class="content" id="homediv">
+      <figure class="image">
+        <img src="~/assets/moscow3.jpg" id="homeimg" />
+        <figcaption id="homeimg-caption">
+          Revolutionary psychologies, communist neurologies, red therapies
+        </figcaption>
+      </figure>
+    </div>
+  </section>
 </template>

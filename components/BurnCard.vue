da0af08190379842da93308eac5ec591bba70184
rnyn <24571853+rnyn@users.noreply.github.com>
2024-03-15 08:10:14 +0100

    adding blurbs with blockquotes

--- components/BurnCard.vue
+++ components/BurnCard.vue
@@ -71,13 +71,28 @@
             </p>
 
             <p> <i>Burnout</i> considers despairing former Communards exiled to a penal colony in the South Pacific; exhausted Bolsheviks recuperating in sanatoria in the aftermath of the October Revolution; an ex-militant on the analyst’s couch relating dreams of ruined landscapes; Chinese peasants engaging in self-criticism sessions; a political organiser seeking advice from a spiritual healer; civil rights movement activists battling weariness; and a group of feminists padding a room with mattresses to scream about the patriarchy. Jettisoning self-help narratives and individualizing therapy talk, Proctor offers a different way forward - neither denial nor despair. Her cogent exploration of the ways militants have made sense of their own burnout demonstrates that it is possible to mourn and organise at once, and to do both without compromise.
+</p>
+
+<!-- blurb goes here -->
+
+              <div class="book-blurbs">
+  <blockquote class="blurb">
+    <p>"Hannah Proctor is one of the best writers on the left today, and this is an extraordinary and extremely timely book – a kaleidoscopic work of revolutionary history on what happens when our day doesn’t come and we have to cope with the consequences. Refusing both the easy temptations of left melancholia and forced ‘just another push, comrades!’ optimism, this is a book full of unromantic communist longing, deadpan humor, and hard-won wisdom."</p>
+    <footer>
+      <cite>Owen Hatherley, author of <i>The Ministry of Nostalgia</i></cite>
+    </footer>
+  </blockquote>
+  <blockquote class="blurb">
+    <p>"Not since Freud first described war neurosis have we been treated to such an astonishing taxonomy of the human mind. In <i>Burnout</i>, Hannah Proctor takes that feeling we all have, and names it again and again, helping us to resee the past and present of revolutionary struggle. A must-read."</p>
+    <footer>
+      <cite>Hannah Zeavin, Founding Editor, <i>Parapraxis</i></cite>
+    </footer>
+  </blockquote>
+</div>
 
 
-
-
-
-      </p>
-      <!-- Blurb goes here -->
+<!-- blurb ends here -->
+     
     </div>
       <nav class="level is-mobile">
         <div class="level-item has-text-centered">
